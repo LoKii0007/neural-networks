831868f7917408fea5d5782c177235e1d86ae066
loki <lokeshyadav1098@gmail.com>
2026-03-16 02:33:37 +0530

    training neural network - forward pass backward pass

--- micrograd/app/neural_network.py
+++ micrograd/app/neural_network.py
@@ -13,6 +13,9 @@ class Neuron:
         out = act.tanh()
         return out
     
+    def parameters(self):
+        return self.w + [self.b]
+    
 class Layer:
     def __init__(self, nin, nout):
         self.neurons = [Neuron(nin) for _ in range(nout)]
@@ -20,6 +23,9 @@ class Layer:
     def __call__(self, x):
         outs = [n(x) for n in self.neurons]
         return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
     
 class MLP:
     def __init__(self, nin, nouts):
@@ -30,6 +36,9 @@ class MLP:
         for layer in self.layers:
             x = layer(x)
         return x
+    
+    def parameters(self):
+        return [l for layer in self.layers for l in layer.parameters()]
         
     
 x = [2.0, 3.0]
@@ -40,7 +49,9 @@ l = Layer(2, 3)
 # l(x)
 
 m = MLP(2, [4, 4, 1])
-# print(m(x))
+print(m(x))
+# params = m.parameters()
+# print(len(params))
 # save_graph(m(x))
 
 xs = [
@@ -52,13 +63,55 @@ xs = [
 
 ys = [1.0, -1.0, -1.0, 1.0]
 m = MLP(3, [4,4,1])
-yc = [m(x) for x in xs]
-print(yc)
 
-# calculating loss to fi thw eights so that nn performs well 
+# print(yc)
 
-loss = sum([ (yc- ye)**2 for yc, ye in zip(yc, ys) ])
-print(loss)
+## manually calculating loss to fi thw eights so that nn performs well 
+# yc = [m(x) for x in xs]
+# params = m.parameters()
+# loss = sum([ (yc- ye)**2 for yc, ye in zip(yc, ys) ])
 
-loss.backward()
-print(m.layers[0].neurons[0].w[0].grad)
+# print('--------')
+# print('before adjusting params')
+
+# loss.backward()
+# # save_graph(loss)
+# print('grad', m.layers[0].neurons[0].w[0].grad)
+# print('data', m.layers[0].neurons[0].w[0].data)
+# print('loss', loss)
+
+# for p in params:
+#     p.data += 0.01 * p.grad
+
+# yc = [m(x) for x in xs]
+# loss = sum([ (yc- ye)**2 for yc, ye in zip(yc, ys) ])
+# print('--------')
+# print('after adjusting params')
+
+# print('grad', m.layers[0].neurons[0].w[0].grad)
+# print('data', m.layers[0].neurons[0].w[0].data)
+
+
+# print('loss',loss)
+
+# creating a loop
+
+
+for k in range(20):
+    
+    # forward pass 
+    yc = [m(x) for x in xs]
+    loss = sum([ (yc- ye)**2 for yc, ye in zip(yc, ys) ])
+    
+    # backward pass 
+    for p in m.parameters(): #for each backward pass we need to reset the grads to 0 otherwise its += so they are being updated with their prev values
+        p.grad = 0.0
+    loss.backward()
+    
+    # update 
+    for p in m.parameters():
+        p.data += 0.05 * p.grad
+        
+    # values
+    print('step - ', k+1, 'loss - ', loss) 
+    print(yc)
